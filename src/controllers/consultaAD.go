55a24f97ba19894eacdc69535a726cf7091b51eb
Leonardo <leonardotandelicw@outlook.com>
2022-06-21 10:43:53 -0300

    fix: ajuste dos comentários e nomenclatura da função de consulta do AD

--- src/controllers/consultaAD.go
+++ src/controllers/consultaAD.go
@@ -11,7 +11,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func CheckAD(w http.ResponseWriter, r *http.Request) {
+// ConsultaAD faz a busca de informações do Active Directory no command prompt e retorna as informações para formatação.
+func ConsultaAD(w http.ResponseWriter, r *http.Request) {
 	//parametros recebe dados através da url
 	parametros := mux.Vars(r)
 	loginNT := parametros["login"]
@@ -98,49 +99,5 @@ func CheckAD(w http.ResponseWriter, r *http.Request) {
 	gpoWithoutSpace := strings.TrimSpace(gpoFormat)
 
 	respostas.JSON(w, http.StatusOK, modelos.DadosAD{LOGIN_NT: loginNTWithoutSpace, NOME: nomeWithoutSpace, CONTA_ATIVA: contaAtivaWithoutSpace, SENHA_ULTIMA_DEFINICAO: senhaDefinicaoWithoutSpace, SENHA_EXPIRACAO: senhaExpiracaoWithoutSpace, DATA_ULTIMO_LOGON: ultimoLogonWithoutSpace, GPO: gpoWithoutSpace})
-
-}
-
-func CheckLAPS(w http.ResponseWriter, r *http.Request) {
-	//parametros recebe dados através da url
-	parametros := mux.Vars(r)
-	locador := parametros["locador"]
-
-	//fmt.Println(locador)
-
-	//comando de de verificação de usuário no domínio.
-	ps := "powershell.exe"
-	cm := "-command"
-	ad := "Get-ADComputer -Identity"
-	ad2 := "-Properties *"
-
-	//executa o comando net user no cmd
-	cmd := exec.Command(ps, cm, ad, locador, ad2)
-	//retorna os dados do cmd
-	cmdOut, err := cmd.Output()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	cmdReturn := string(cmdOut)
-
-	//	fmt.Println(cmdReturn)
-
-	//Define onde estará a informação
-	var senhaWithoutSpace = ""
-	if strings.Contains(cmdReturn, "ms-Mcs-AdmPwd") == true {
-		SenhaSliceInitial := strings.Index(cmdReturn, "ms-Mcs-AdmPwd")
-		SenhaSliceEnding := strings.Index(cmdReturn, `ms-Mcs-AdmPwdExpirationTime`)
-		//fatia os dados com a informação recebida acima
-		senha := string(cmdReturn)[SenhaSliceInitial+38 : SenhaSliceEnding]
-		//Formatação - Remoção dos espaços
-		senhaWithoutSpace = strings.TrimSpace(senha)
-		//	fmt.Println(senhaWithoutSpace)
-	} else {
-		senhaWithoutSpace = "Nenhuma senha encontrada"
-	}
-
-	respostas.JSON(w, http.StatusOK, modelos.DadosLAPS{SENHA: senhaWithoutSpace})
 
 }
